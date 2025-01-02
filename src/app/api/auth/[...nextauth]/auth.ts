977d3a5d69b88974ef77958c4b3aaebf9657abda
Acaj0 <antoniocarlosjr007@hotmail.com>
2025-01-02 14:03:53 -0400

    13

---
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -0,0 +1,55 @@
+import { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        console.log("Authorize function called")
+        
+        if (!credentials?.username || !credentials?.password) {
+          console.error("Missing credentials")
+          throw new Error("Credenciais inválidas")
+        }
+
+        console.log("Checking credentials")
+        console.log("Username provided:", credentials.username)
+        console.log("Expected username:", process.env.ADMIN_USERNAME)
+
+        if (credentials.username === process.env.ADMIN_USERNAME && 
+            credentials.password === process.env.ADMIN_PASSWORD) {
+          console.log("Credentials match, returning user")
+          return { id: "1", name: "Admin" }
+        }
+        
+        console.log("Credentials do not match")
+        return null
+      }
+    })
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    }
+  },
+  pages: {
+    signIn: '/login',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: true,
+}
+
